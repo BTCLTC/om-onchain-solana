e54623aea56ba466f27067bfbef52658e54d6821
Zihua Liu <zliu19@stanford.edu>
2021-10-26 21:37:01 -0700

    restruct

--- programs/om-universe/src/lib.rs
+++ programs/om-universe/src/lib.rs
@@ -1,13 +1,5 @@
 use anchor_lang::prelude::*;
 use mint::*;
-use {
-  anchor_lang::{
-      solana_program::program::invoke, AnchorDeserialize, AnchorSerialize, Key,
-  },
-  spl_token_metadata::{
-    instruction::{create_master_edition, create_metadata_accounts, update_metadata_accounts},
-  },
-};
 
 mod mint;
 
@@ -20,10 +12,38 @@ pub mod om_universe {
   pub fn mint_genesis_node<'info>(
     ctx: Context<'_, '_, '_, 'info, MintGenesisNode<'info>>,
     data: NftConfigData,
-    bump_node: u8,
+    node_bump: u8,
+    gensis_bump: u8
   ) -> ProgramResult {
-    // TODO: mint the gensis node
-    
+    // TODO: if the genesis account is minted already, throw error
+
+    // mint contribution as NFT
+    mint_nft(
+      &ctx.accounts.treasury,
+      &ctx.accounts.payer,
+      &ctx.accounts.metadata,
+      &ctx.accounts.mint,
+      &ctx.accounts.mint_authority,
+      &ctx.accounts.update_authority,
+      &ctx.accounts.master_edition,
+      &ctx.accounts.token_metadata_program,
+      &ctx.accounts.system_program,
+      &ctx.accounts.token_program,
+      &ctx.accounts.rent,
+      data.name,
+      data.symbol,
+      data.uri,
+    )?;
+
+    // store the node informations
+    ctx.accounts.node_account.community_type = String::from("COMMUNITY");
+    ctx.accounts.node_account.is_genesis = true;
+    ctx.accounts.node_account.parent = None;
+    ctx.accounts.node_account.creator = ctx.accounts.payer.key();
+
+    // update genesis information
+    ctx.accounts.genesis_node.is_minted = true;
+    ctx.accounts.genesis_node.creator = ctx.accounts.payer.key();
     Ok(())
   }
 
@@ -32,100 +52,28 @@ pub mod om_universe {
     data: NftConfigData,
     bump_node: u8,
   ) -> ProgramResult {
-    let creators: Vec<spl_token_metadata::state::Creator> =
-      vec![spl_token_metadata::state::Creator {
-      address: ctx.accounts.treasury.key(),
-        verified: false,
-        share: 3,
-      }, spl_token_metadata::state::Creator {
-        address: ctx.accounts.payer.key(),
-        verified: false,
-        share: 97
-      }
-    ];
-
-    let metadata_infos = vec![
-      ctx.accounts.metadata.clone(),
-      ctx.accounts.mint.clone(),
-      ctx.accounts.mint_authority.clone(),
-      ctx.accounts.update_authority.clone(),
-      ctx.accounts.payer.clone(),
-      ctx.accounts.token_metadata_program.clone(),
-      ctx.accounts.token_program.clone(),
-      ctx.accounts.system_program.clone(),
-      ctx.accounts.rent.to_account_info().clone(),
-    ];
-
-    let master_edition_infos = vec![
-      ctx.accounts.master_edition.clone(),
-      ctx.accounts.mint.clone(),
-      ctx.accounts.mint_authority.clone(),
-      ctx.accounts.update_authority.clone(),
-      ctx.accounts.payer.clone(),
-      ctx.accounts.metadata.clone(),
-      ctx.accounts.token_metadata_program.clone(),
-      ctx.accounts.token_program.clone(),
-      ctx.accounts.system_program.clone(),
-      ctx.accounts.rent.to_account_info().clone(),
-    ];
-
-    let create_meta_ix = create_metadata_accounts(
-      *ctx.accounts.token_metadata_program.key,
-      *ctx.accounts.metadata.key,
-      *ctx.accounts.mint.key,
-      *ctx.accounts.mint_authority.key,
-      *ctx.accounts.payer.key,
-      *ctx.accounts.update_authority.key,
+    mint_nft(
+      &ctx.accounts.treasury,
+      &ctx.accounts.payer,
+      &ctx.accounts.metadata,
+      &ctx.accounts.mint,
+      &ctx.accounts.mint_authority,
+      &ctx.accounts.update_authority,
+      &ctx.accounts.master_edition,
+      &ctx.accounts.token_metadata_program,
+      &ctx.accounts.system_program,
+      &ctx.accounts.token_program,
+      &ctx.accounts.rent,
       data.name,
       data.symbol,
       data.uri,
-      Some(creators),
-      0,
-      true,
-      false,
-    );
-    invoke(
-      &create_meta_ix,
-      metadata_infos.as_slice(),
     )?;
 
-    invoke(
-      &create_master_edition(
-        *ctx.accounts.token_metadata_program.key,
-        *ctx.accounts.master_edition.key,
-        *ctx.accounts.mint.key,
-        *ctx.accounts.update_authority.key,
-        *ctx.accounts.mint_authority.key,
-        *ctx.accounts.metadata.key,
-        *ctx.accounts.payer.key,
-        Some(0),
-      ),
-      master_edition_infos.as_slice(),
-    )?;
-    
-    invoke(
-      &update_metadata_accounts(
-        *ctx.accounts.token_metadata_program.key,
-        *ctx.accounts.metadata.key,
-        *ctx.accounts.update_authority.key,
-        Some(ctx.accounts.update_authority.key()),
-        None,
-        Some(true),
-      ),
-      &[
-        ctx.accounts.update_authority.clone(),
-        ctx.accounts.token_metadata_program.clone(),
-        ctx.accounts.metadata.clone(),
-      ],
-    )?;
-
-    // Store graph data structure
-    ctx.accounts.node_account.community_type = String::from("node_account");
+    // store the node informations
+    ctx.accounts.node_account.community_type = String::from("COMMUNITY");
     ctx.accounts.node_account.is_genesis = false;
     ctx.accounts.node_account.parent = Some(ctx.accounts.parent_token_account.key());
-
-    // TODO: figure out how to store graph data
-
+    ctx.accounts.node_account.creator = ctx.accounts.payer.key();
     Ok(())
   }
 
@@ -137,110 +85,4 @@ pub mod om_universe {
     ctx.accounts.graph_node.community_type = community_type;
     Ok(())
   }
-}
-
-fn mint_nft<'a>(
-  treasury: AccountInfo<'a>,
-  payer: AccountInfo<'a>,
-  metadata: AccountInfo<'a>,
-  mint: AccountInfo<'a>,
-  mint_authority: AccountInfo<'a>,
-  update_authority: AccountInfo<'a>,
-  master_edition: AccountInfo<'a>,
-  token_metadata_program: AccountInfo<'a>,
-  system_program: AccountInfo<'a>,
-  token_program: AccountInfo<'a>,
-  rent: Sysvar<'a, Rent>,
-  name: String,
-  symbol: String,
-  uri: String
-) -> ProgramResult {
-  let creators: Vec<spl_token_metadata::state::Creator> =
-      vec![spl_token_metadata::state::Creator {
-      address: treasury.key(),
-        verified: false,
-        share: 3,
-      }, spl_token_metadata::state::Creator {
-        address: payer.key(),
-        verified: false,
-        share: 97
-      }
-    ];
-
-    let metadata_infos = vec![
-      metadata.clone(),
-      mint.clone(),
-      mint_authority.clone(),
-      update_authority.clone(),
-      payer.clone(),
-      token_metadata_program.clone(),
-      token_program.clone(),
-      system_program.clone(),
-      rent.to_account_info().clone(),
-    ];
-
-    let master_edition_infos = vec![
-      master_edition.clone(),
-      mint.clone(),
-      mint_authority.clone(),
-      update_authority.clone(),
-      payer.clone(),
-      metadata.clone(),
-      token_metadata_program.clone(),
-      token_program.clone(),
-      system_program.clone(),
-      rent.to_account_info().clone(),
-    ];
-
-    let create_meta_ix = create_metadata_accounts(
-      token_metadata_program.key(),
-      metadata.key(),
-      mint.key(),
-      mint_authority.key(),
-      payer.key(),
-      update_authority.key(),
-      name,
-      symbol,
-      uri,
-      Some(creators),
-      0,
-      true,
-      false,
-    );
-    invoke(
-      &create_meta_ix,
-      metadata_infos.as_slice(),
-    )?;
-
-    invoke(
-      &create_master_edition(
-        token_metadata_program.key(),
-        master_edition.key(),
-        mint.key(),
-        update_authority.key(),
-        mint_authority.key(),
-        metadata.key(),
-        payer.key(),
-        Some(0),
-      ),
-      master_edition_infos.as_slice(),
-    )?;
-    
-    invoke(
-      &update_metadata_accounts(
-        token_metadata_program.key(),
-        metadata.key(),
-        update_authority.key(),
-        Some(update_authority.key()),
-        None,
-        Some(true),
-      ),
-      &[
-        update_authority.clone(),
-        token_metadata_program.clone(),
-        metadata.clone(),
-      ],
-    )?;
-
-    Ok(())
 }
--- programs/om-universe/src/mint.rs
+++ programs/om-universe/src/mint.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::system_program;
+use anchor_lang::solana_program::{system_program, program::invoke};
+use spl_token_metadata::{
+  instruction::{create_master_edition, create_metadata_accounts, update_metadata_accounts},
+};
 
 #[derive(Accounts)]
 #[instruction(data: NftConfigData, bump_node:u8)]
@@ -37,7 +40,7 @@ pub struct MintNode<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(data: NftConfigData, bump_node:u8)]
+#[instruction(data: NftConfigData, node_bump:u8, genesis_bump: u8)]
 pub struct MintGenesisNode<'info> {
   // mint vars
   #[account(mut, signer)]
@@ -57,9 +60,11 @@ pub struct MintGenesisNode<'info> {
   #[account(mut)]
   pub token_account: AccountInfo<'info>,
 
-  // story lineage and data vars
-  #[account(init, seeds=["node_account".as_bytes(), token_account.key().as_ref()], payer=payer, bump=bump_node, space=GRAPH_NODE_STRUCT_SIZE)]
+  // node lineage
+  #[account(init, seeds=["node_account".as_bytes(), token_account.key().as_ref()], payer=payer, bump=node_bump, space=GRAPH_NODE_STRUCT_SIZE)]
   pub node_account: ProgramAccount<'info, GraphNode>,
+  #[account(init, seeds=["node_account".as_bytes(), "gensis".as_bytes()], payer=payer, bump=genesis_bump, space=1+32)]
+  pub genesis_node: ProgramAccount<'info, GraphGenesis>,
 
   // system/program vars
   pub token_metadata_program: AccountInfo<'info>, // spl_token_metadata from crate is actually broken!
@@ -70,12 +75,10 @@ pub struct MintGenesisNode<'info> {
   pub rent: Sysvar<'info, Rent>,
 }
 
-
-
 #[derive(Accounts)]
 pub struct ChangeNodeType<'info> {
   #[account(mut)]
-  pub graph_node: ProgramAccount<'info, GraphNode>,
+   pub graph_node: ProgramAccount<'info, GraphNode>,
 }
 
 /// Supporting data structure
@@ -85,6 +88,12 @@ pub struct GraphNode {
   pub creator: Pubkey,        // 32
   pub is_genesis: bool,       // 1
   pub community_type: String, // 24 
+}
+
+#[account]
+pub struct GraphGenesis {
+  pub is_minted: bool, // 1,
+  pub creator: Pubkey, // 32,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
@@ -105,3 +114,110 @@ pub const GRAPH_NODE_STRUCT_SIZE: usize =  32 + // creator
 32 + // parent
 1 + // is_genesis
 24;   // community type
+
+
+pub fn mint_nft<'a>(
+  treasury: &AccountInfo<'a>,
+  payer: &AccountInfo<'a>,
+  metadata: &AccountInfo<'a>,
+  mint: &AccountInfo<'a>,
+  mint_authority: &AccountInfo<'a>,
+  update_authority: &AccountInfo<'a>,
+  master_edition: &AccountInfo<'a>,
+  token_metadata_program: &AccountInfo<'a>,
+  system_program: &AccountInfo<'a>,
+  token_program: &AccountInfo<'a>,
+  rent: &Sysvar<'a, Rent>,
+  name: String,
+  symbol: String,
+  uri: String
+) -> ProgramResult {
+  let creators: Vec<spl_token_metadata::state::Creator> =
+      vec![spl_token_metadata::state::Creator {
+      address: treasury.key(),
+        verified: false,
+        share: 3,
+      }, spl_token_metadata::state::Creator {
+        address: payer.key(),
+        verified: false,
+        share: 97
+      }
+    ];
+
+    let metadata_infos = vec![
+      metadata.clone(),
+      mint.clone(),
+      mint_authority.clone(),
+      update_authority.clone(),
+      payer.clone(),
+      token_metadata_program.clone(),
+      token_program.clone(),
+      system_program.clone(),
+      rent.to_account_info().clone(),
+    ];
+
+    let master_edition_infos = vec![
+      master_edition.clone(),
+      mint.clone(),
+      mint_authority.clone(),
+      update_authority.clone(),
+      payer.clone(),
+      metadata.clone(),
+      token_metadata_program.clone(),
+      token_program.clone(),
+      system_program.clone(),
+      rent.to_account_info().clone(),
+    ];
+
+    let create_meta_ix = create_metadata_accounts(
+      token_metadata_program.key(),
+      metadata.key(),
+      mint.key(),
+      mint_authority.key(),
+      payer.key(),
+      update_authority.key(),
+      name,
+      symbol,
+      uri,
+      Some(creators),
+      0,
+      true,
+      false,
+    );
+    invoke(
+      &create_meta_ix,
+      metadata_infos.as_slice(),
+    )?;
+
+    invoke(
+      &create_master_edition(
+        token_metadata_program.key(),
+        master_edition.key(),
+        mint.key(),
+        update_authority.key(),
+        mint_authority.key(),
+        metadata.key(),
+        payer.key(),
+        Some(0),
+      ),
+      master_edition_infos.as_slice(),
+    )?;
+    
+    invoke(
+      &update_metadata_accounts(
+        token_metadata_program.key(),
+        metadata.key(),
+        update_authority.key(),
+        Some(update_authority.key()),
+        None,
+        Some(true),
+      ),
+      &[
+        update_authority.clone(),
+        token_metadata_program.clone(),
+        metadata.clone(),
+      ],
+    )?;
+
+    Ok(())
+}
